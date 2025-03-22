84d93fadb43cdfb85a36ceabd0230356c498b4fc
1107c <dotoryman@gmail.com>
2025-03-22 20:26:08 +0900

    oauth

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -2,17 +2,14 @@
 import express from "express";
 import passport from "passport";
 import {
-  getLoginPage,
   logout,
   googleCallback,
-  getProfile,
 } from "../controllers/authController.js";
 import { isLoggedIn } from "../middlewares/auth.js";
 
 const authRouter = express.Router();
 
-// Login page
-authRouter.get("/login", getLoginPage);
+
 
 // Google OAuth routes
 authRouter.get(
@@ -29,7 +26,6 @@ authRouter.get(
 // Logout route
 authRouter.get("/logout", logout);
 
-// Profile route (protected)
-authRouter.get("/profile", isLoggedIn, getProfile);
+
 
 export default authRouter;
