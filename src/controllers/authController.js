69dfbbcb2f9dadaad84f82b426c471b39cdf64d6
1107c <dotoryman@gmail.com>
2025-03-22 20:26:08 +0900

    oauth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,28 +1,15 @@
 // src/controllers/authController.js
-export const getLoginPage = (req, res) => {
-    return res.render("login", { pageTitle: "로그인" });
-  };
-  
-  export const logout = (req, res, next) => {
-    req.logout((err) => {
-      if (err) {
-        return next(err);
-      }
-      res.redirect("/");
-    });
-  };
-  
-  export const googleCallback = (req, res) => {
-    // Successful authentication, redirect home
-    res.redirect("/");
-  };
-  
-  export const getProfile = (req, res) => {
-    if (!req.user) {
-      return res.redirect("/auth/login");
+
+export const logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
     }
-    return res.render("profile", { 
-      pageTitle: "사용자 프로필",
-      user: req.user 
-    });
-  };
+    res.redirect("/");
+  });
+};
+
+export const googleCallback = (req, res) => {
+  // Successful authentication, redirect home
+  res.redirect("/");
+};
